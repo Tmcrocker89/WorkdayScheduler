c3281c0039670a0a4f031ffb924d0e3cb58dbd27
Travis Crocker <Travis.Crocker@outlook.com>
2021-09-24 14:34:46 -0400

    added date and time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,8 @@
+let now = moment().format('dddd, MMMM Do');
+let dateEl = $("#dateAndTime");
+
+dateEl.text(now)
+
 $(document).on("click", ".btn", function()
 {
     console.log($(this).siblings("input").val())
